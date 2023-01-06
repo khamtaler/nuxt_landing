4440a2ca6eda85f27af94bd3f17d7ed8302b46e8
Tomasz Sroka <tsroka41@gmail.com>
2023-01-06 14:06:32 +0100

    simple navbar construction and default layout setup with initialization of the footer

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+	<header>
+		<nav>
+			<NuxtLink to="/"><img src="#" /></NuxtLink>
+			<ul>
+				<li><NuxtLink to="/">Projects</NuxtLink></li>
+				<li><NuxtLink to="/">Partners</NuxtLink></li>
+				<li><NuxtLink to="/">FAQ</NuxtLink></li>
+				<li><NuxtLink to="/">Masters</NuxtLink></li>
+				<li><NuxtLink to="/">Classes</NuxtLink></li>
+				<li><NuxtLink to="/">Course experience</NuxtLink></li>
+				<li><NuxtLink to="/">About us</NuxtLink></li>
+			</ul>
+			<button>Register</button>
+		</nav>
+	</header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
